7027be2cd82e6e8e6b953c34625b035966b77d12
Claudio Tubertini <claudio.tubertini@gmail.com>
2017-01-06 23:50:59 +0100

    update

--- js/corticellaapp.js
+++ js/corticellaapp.js
@@ -5,18 +5,6 @@
 
 
 
-// You can avoid the document.ready if you put the script at the bottom of the page
-$(document).ready(function() {
-  // Bind to the click of all links with a #hash in the href
-  $('a[href^="#"]').click(function(e) {
-    // Prevent the jump and the #hash from appearing on the address bar
-    e.preventDefault();
-    // Scroll the window, stop any previous animation, stop on user manual scroll
-    // Check https://github.com/flesler/jquery.scrollTo for more customizability
-    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true});
-  });
-});
-
 
 /////////////////
 
@@ -110,7 +98,7 @@ var init = function () {
             showWeek: true
         });
         //apply ko bindings
-        ko.applyBindings(ShiftWork);
+        ko.applyBindings(ShiftWork, document.getElementById('contatti'));
     };
 
     /* execute the init function when the DOM is ready */
