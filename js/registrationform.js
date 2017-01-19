ba24e6daf95a984273679dc56614446f78573fe3
Claudio Tubertini <claudio.tubertini@gmail.com>
2017-01-19 23:32:55 +0100

    form update

--- js/registrationform.js
+++ js/registrationform.js
@@ -6,10 +6,41 @@ var RegistrationForm = function () {
     lastName: ko.observable()
   },
   contactDetails: {
-    phoneNumber: ko.observable(),
     emailAddress: ko.observable()
   }
 };
+/* extender for required fields */
+ko.extenders.required = function(target, option) {
+  //observables to indicate an error
+  target.hasError = ko.observable(false);
+
+  //set the error flag whenever the value changes
+  target.subscribe(function (newValue) {
+     target.hasError(newValue ? false : true);
+  });
+
+  //return the original observable
+  return target;
+};
+/* method to traverse the model and clear observables */
+  var traverseAndClearModel = function(jsonObj) {
+    $.each(jsonObj, function(key,val){
+      if(ko.isObservable(val)) {
+        if(val.removeAll != undefined) {
+          val.removeAll();
+        } else {
+          val(null);
+        }
+      } else {
+        traverseAndClearModel(val);
+      }
+    });
+  };
+  /* clear the model */
+  var clear = function () {
+    console.log("Clear customer model");
+    traverseAndClearModel(customer);
+  };
   /* form submission */
     var submit = function () {
       console.log(ko.toJSON(customer));
@@ -24,6 +55,7 @@ var RegistrationForm = function () {
   $(init);
 
   return {
+    clear: clear,
     customer: customer,
     submit: submit
   };
