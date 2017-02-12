02d169f70f63e224c88796437624cfb58a69eb16
Claudio Tubertini <claudio.tubertini@gmail.com>
2017-02-12 21:52:21 +0100

    update YQL ajax

--- js/newsapp2.js
+++ js/newsapp2.js
@@ -28,32 +28,32 @@ var RetrieveNews = function (){
 
   /* product observable array */
   self.products = ko.observableArray();
-  $.ajax({
-    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.agi.it%2Fsalute%2Frss%22&format=json&diagnostics=false&callback=',
-    dataType: 'json',
-    success: function(data) {
-                var items = data.query.results.item;
+  // $.ajax({
+  //   url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.agi.it%2Fsalute%2Frss%22&format=json&diagnostics=false&callback=',
+  //   dataType: 'json',
+  //   success: function(data) {
+  //               var items = data.query.results.item;
                 
-                var no_items=items.length;
-                for(var i=0;i<no_items;i++){
-                  var title = items[i].title;
-                  var link = items[i].link;
-                  var date = items[i].puDate;
-                  var enclosure = items[i].enclosure.url;
-                  self.products.push(new productModel({
-                    title: title,
-                    date: date,
-                    link: link,
-                    enclosure: enclosure
-                     }));
-                  console.log(items[i]);
-                  //self.tasks.push(new Task({ title: this.newTaskText() }));
-                };
-              },
-    error: function(){
-            alert("Abbiamo riscontrato un problema nell\'aggiornamento delle notizie. Riprovate fra qualche minuto.");
-        }
-    });
+  //               var no_items=items.length;
+  //               for(var i=0;i<no_items;i++){
+  //                 var title = items[i].title;
+  //                 var link = items[i].link;
+  //                 var date = items[i].puDate;
+  //                 var enclosure = items[i].enclosure.url;
+  //                 self.products.push(new productModel({
+  //                   title: title,
+  //                   date: date,
+  //                   link: link,
+  //                   enclosure: enclosure
+  //                    }));
+  //                 console.log(items[i]);
+  //                 //self.tasks.push(new Task({ title: this.newTaskText() }));
+  //               };
+  //             },
+  //   error: function(){
+  //           alert("Abbiamo riscontrato un problema nell\'aggiornamento delle notizie. Riprovate fra qualche minuto.");
+  //       }
+  //   });
 
    // $(data).find("item").each(function () { // or "item" or whatever suits your feed
    //            var el = $(this);
@@ -64,37 +64,27 @@ var RetrieveNews = function (){
    //              "enclosure": el.find("enclosure").attr('url')};
    //            products.push(new productModel(item));
    //            });
-// $.ajax({
-//     url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.agi.it%2Fsalute%2Frss%22&format=json&diagnostics=false&callback=',
-//     dataType: 'json',
-//     success: function(data) {
-//       $(data).find(results.item).each(function () { // or "item" or whatever suits your feed
-//               var el = $(this);
-//               it = {
-//                 "title": el.find("title").text(),
-//                 "date": el.find("pubDate").text(),
-//                 "link": el.find("link").text(),
-//                 "enclosure": el.find("enclosure").attr('url')};
-//               products.push(new productModel(it));
-//               });
-//         },
-//     error: function(){
-//             alert("Abbiamo riscontrato un problema nell\'aggiornamento delle notizie. Riprovate fra qualche minuto.");
-//         }
-//     });
+$.ajax({
+    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%3D%22http%3A%2F%2Fwww.agi.it%2Fsalute%2Frss%22&format=json&diagnostics=false&callback=',
+    dataType: 'json',
+    success: function(o) {
+      $.each(o.query.results.item, function () { // or "item" or whatever suits your feed
+              var el = $(this);
+              it = {
+                "title": el.find("title").text(),
+                "date": el.find("pubDate").text(),
+                "link": el.find("link").text(),
+                "enclosure": el.find("enclosure").attr('url')};
+              self.products.push(new productModel(it));
+              
+              });
+        },
+    error: function(){
+            alert("Abbiamo riscontrato un problema nell\'aggiornamento delle notizie. Riprovate fra qualche minuto.");
+        }
+    });
 
-// function top_stories(o){
-//         var items = o.query.results.item;
-//         var output = '';
-//         var no_items=items.length;
-//         for(var i=0;i<no_items;i++){
-//           var title = items[i].title;
-//           var link = items[i].link;
-//           var desc = items[i].description;
-//           output += "<h3><a href='" + link + "'>"+title+"</a></h3>" + desc + "<hr/>";
-//         }
-//         document.getElementById('results').innerHTML = output;
-//       }
+
 
   self.formattedDate = ko.pureComputed(function(){
             return mydate(self.data.date());
